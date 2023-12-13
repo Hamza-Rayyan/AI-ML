ccfe4f1f7a237b6fceb2518b182cd437c34214f7
SYED HAMZAH RAYYAN <141110144+Hamza-Rayyan@users.noreply.github.com>
2023-12-13 21:50:05 +0530

    Add files via upload

---
+++ Water Mark/ouptut_code.py
@@ -0,0 +1,11 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:\Python311\Water Mark Project\add_watermark.py ==========
+79
+608
+Adding watermark
+1600
+1236
+Watermark added to all the images
+Project End
